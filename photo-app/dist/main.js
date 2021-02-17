0d4c39797891fb80afd62d1f5e8879085bd37d5a
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-02-16 20:54:06 -0600

    Clase 29

--- photo-app/dist/main.js
+++ photo-app/dist/main.js
@@ -1,10 +1,16 @@
 "use strict";
+// import { User, Album, Picture, PhotoOrientation } from './photo-app';
 Object.defineProperty(exports, "__esModule", { value: true });
-var photo_app_1 = require("./photo-app");
-var user = new photo_app_1.User(1, 'aeruiz', 'Andres', true);
-var album = new photo_app_1.Album(10, 'Platzi Album');
-var picture = new photo_app_1.Picture(1, 'TypeScript Course', '2021-02-01', photo_app_1.PhotoOrientation.Panorama);
+const album_1 = require("./album");
+const photo_orientation_1 = require("./photo-orientation");
+const picture_1 = require("./picture");
+const user_1 = require("./user");
+const user = new user_1.User(1, 'aeruiz', 'Andres', true);
+const album = new album_1.Album(10, 'Platzi Album');
+const picture = new picture_1.Picture(1, 'TypeScript Course', '2021-02-01', photo_orientation_1.PhotoOrientation.Panorama);
 // Creamos relaciones
 user.addAlbum(album);
 album.addPicture(picture);
+console.log('user', user);
+user.removeAlbum(album);
 console.log('user', user);
--- photo-app/src/main.ts
+++ photo-app/src/main.ts
@@ -1,4 +1,9 @@
-import { User, Album, Picture, PhotoOrientation } from './photo-app';
+// import { User, Album, Picture, PhotoOrientation } from './photo-app';
+
+import { Album } from "./album";
+import { PhotoOrientation } from "./photo-orientation";
+import { Picture } from "./picture";
+import { User } from "./user";
 
 const user = new User(1, 'aeruiz', 'Andres', true);
 const album = new Album(10, 'Platzi Album');
@@ -7,4 +12,6 @@ const picture = new Picture(1, 'TypeScript Course', '2021-02-01', PhotoOrientati
 user.addAlbum(album);
 album.addPicture(picture);
 
+console.log('user', user);
+user.removeAlbum(album);
 console.log('user', user);
